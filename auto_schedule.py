d636bc36883b091d717e068e77b840194a1b62ba
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2023-04-19 20:19:25 +0000

    Added schedule file and skeleton structure.

---
+++ auto_schedule.py
@@ -0,0 +1,59 @@
+import sys
+import argparse
+import networkx as nx
+import glpk
+
+#Defining functions for pre-processing the graph representation and design specifications
+def read_edgelist(file_path):
+    G = nx.read_edgelist(file_path, delimiter='',data=[('memory', int)])
+    return G
+
+def preprocess_graph(G):
+    #implement any required pre-processing here
+    pass
+
+def preprocess_design_specification(latency, memory):
+    #implement any required pre-processing here
+    pass
+
+#Defining functions for generating ILP formulations and solving them using the GLPK solver
+def generate_ilp_formulation(G, latency, memory):
+    #implement ILP formulation generation here
+    pass
+
+def solve_ilp_formulation(ilp_formulation):
+    #implement ILP solution extraction using GLPK solver here
+    pass
+
+#Defining functions for handling the scheduling objectives
+def minimize_memory_under_latency(G, L):
+    #implement memory minimization under latency L here
+    pass
+
+def minimize_latency_under_memory(G, M):
+    #impliment latency minimization under memory M here
+    pass
+
+def latency_memory_pareto_analysis(G, L, M):
+    #implement Pareto-optimal analysis here
+    pass
+
+#Main function to parse input arguments and execute the tool
+def main():
+    parser = argparse.ArgumentParser(description='Automated ILP Scheduling Tool')
+    parser.add_argument('-l', type=int, help='Latency L')
+    parser.add_argument('-a', type=int, help='Memory M')
+    parser.add_argument('-g', type=str, help='Path to the edgelist file')
+
+    args = parser.parse_args()
+
+    G = read_edgelist(args.g)
+    preprocess_graph(G)
+    preprocess_design_specifications(args.l, args.a)
+
+    minimize_memory_under_latency(G, args.l)
+    minimize_latency_under_memory(G, args.a)
+    latency_memory_pareto_analysis(G, args.l, args.a)
+
+    if __name__ == "__main__":
+        main()
