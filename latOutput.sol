42d9e5241c57b5b4036fba5786e3499da345dcbe
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2023-04-30 23:35:39 +0000

    Have seperate files being made

---
+++ latOutput.sol
@@ -0,0 +1,59 @@
+Problem:    
+Rows:       31
+Columns:    4 (4 integer, 0 binary)
+Non-zeros:  94
+Status:     INTEGER EMPTY
+Objective:  obj = 0 (MINimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 c0                          0             1             = 
+     2 c1                          0             1             = 
+     3 c2                          0             1             = 
+     4 c3                          0             1             = 
+     5 c4                          0             1               
+     6 c5                          0             1               
+     7 c6                          0             1               
+     8 c7                          0                          20 
+     9 c8                          0                          20 
+    10 c9                          0                          20 
+    11 c10                         0                          20 
+    12 c11                         0                          20 
+    13 c12                         0                          20 
+    14 c13                         0                          20 
+    15 c14                         0                          20 
+    16 c15                         0                          20 
+    17 c16                         0                          20 
+    18 c17                         0                          20 
+    19 c18                         0                          20 
+    20 c19                         0                          20 
+    21 c20                         0                          20 
+    22 c21                         0                          20 
+    23 c22                         0                          20 
+    24 c23                         0                          20 
+    25 c24                         0                          20 
+    26 c25                         0                          20 
+    27 c26                         0                          20 
+    28 i0                          0             0               
+    29 i1                          0             0               
+    30 i2                          0             0               
+    31 i3                          0             0               
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 x1           *              0             0            20 
+     2 x2           *              0             0            20 
+     3 x3           *              0             0            20 
+     4 x4           *              0             0            20 
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 1.00e+00 on row 1
+        max.rel.err = 5.00e-01 on row 1
+        SOLUTION IS INFEASIBLE
+
+End of output
--- src/test_auto.py
+++ src/test_auto.py
@@ -1,10 +1,11 @@
+
 import sys
 import argparse
 import networkx as nx
 import glpk
 import subprocess
 
-#To run the program: python3 src/auto_schedule.py -l=5 -a=20 -g=test/test1.edgelist
+#To run the program: python3 src/test_auto.py -l=5 -a=20 -g=test/test1.edgelist
 
 #Defining functions for pre-processing the graph representation and design specifications
 def read_edgelist(file_path):
@@ -16,6 +17,10 @@ def preprocess_graph(G):
     rootNode = -1 #Topmost node created before getting into the actual graph (represented as a -1)
     G.add_node(rootNode)
     return G, rootNode
+
+def preprocess_design_specification(latency, memory):
+    #implement any required pre-processing here
+    pass
 
 #Defining functions for generating ILP formulations and solving them using the GLPK solver
 def generate_ilp_formulation(G, latency, memory):
@@ -55,39 +60,7 @@ def generate_ilp_formulation(G, latency, memory):
     
     for n in G:
         nodePred = list(G.predecessors(n))
-        if len(nodePred) == 0: # In case the node has no predecessors (top level)
-            G.nodes[n]["TotalWeight"] = 0 # Top level nodes have a weight/memory of 0
-        else:
-            total_weight = 0
-            for np in nodePred:
-                edge_weight = G[np][n]["weight"]
-                total_weight += edge_weight
-            G.nodes[n]["TotalWeight"] = int(total_weight)
-            
-    #TODO: PUT IN MINIMIZATION HERE AFTER WEIGHTS ARE CALCULATED
-    min_Mem = minimize_memory_under_latency(G, latency)
-    print("Min Mem under latency Output: ", min_Mem)
         
-    min_Lat = minimize_latency_under_memory(G, memory, latency)
-    print("Min Latency Output: ", min_Lat)
-    
-    filesMade = []
-    filesMade.append(create_ilp_file(G, min_Mem, True, memory))
-    filesMade.append(create_ilp_file(G, min_Lat, False, memory))
-    
-    return filesMade
-            
-def create_ilp_file(G, schedule, memoryMinTrue, memory):
-    #Implement ILP file maker that uses schedule dictionary for its constraints
-    #---ILP File Creation---  TODO: MOVE THIS INTO ITS OWN METHOD
-    if(memoryMinTrue == True):
-        ilp_file = "memoryMin.ilp"
-    else:
-        ilp_file = "latencyMin.ilp"
-    
-    rootNode = -1
-    for n in G:
-        nodePred = list(G.predecessors(n))
         if len(nodePred) == 0: # In case the node has no predecessors (top level)
             G.nodes[n]["TotalWeight"] = 0 # Top level nodes have a weight/memory of 0
         else:
@@ -95,8 +68,12 @@ def create_ilp_file(G, schedule, memoryMinTrue, memory):
             for np in nodePred:
                 edge_weight = G[np][n]["weight"]
                 total_weight += edge_weight
+                
             G.nodes[n]["TotalWeight"] = int(total_weight)
-    
+            #print(n, G.nodes[n]["TotalWeight"])
+                
+    #---ILP File Creation---  TODO: MOVE THIS INTO ITS OWN METHOD
+    ilp_file = "pred.ilp"
     with open(ilp_file, 'w') as f: #'w' lets you write (overwrite) a file while 'a' lets you append/add to a file
         constraintNum = 0
         constraint = "c"
@@ -108,6 +85,13 @@ def create_ilp_file(G, schedule, memoryMinTrue, memory):
         f.write("obj: " + " + ".join(obj_terms) + "\n")
             
         f.write("\nSubject To\n")
+    
+        #TODO: PUT IN MINIMIZATION HERE AFTER WEIGHTS ARE CALCULATED
+        min_Mem = minimize_memory_under_latency(G, latency)
+        print("Min Mem under latency Output: ", min_Mem)
+        
+        min_Lat = minimize_latency_under_memory(G, memory, latency)
+        print("Min Latency Output: ", min_Lat)
         
         #Each node constraint
         for n in G:
@@ -167,15 +151,33 @@ def create_ilp_file(G, schedule, memoryMinTrue, memory):
         
     return ilp_file
 
-def solve_ilp_formulation(ilp_formulation_file, minMemTrue):
+def solve_ilp_formulation(ilp_formulation_file):
     #implement ILP solution extraction using GLPK solver here
-    if minMemTrue == True:
-        output_file = "memOutput.sol"
-    else:
-        output_file = "latOutput.sol"
-        
+    output_file = "output.sol"
     result = subprocess.run(["glpsol", "--lp", ilp_formulation_file, "-o", output_file])
     return output_file, result.returncode
+
+#Defining functions for handling the scheduling objectives
+# def minimize_memory_under_latency(G, L): 
+#     #implement memory minimization under latency L here
+#     asapRes = calculate_asap(G, L)
+#     alapRes = calculate_alap(G, L)
+#     slackRes = compute_slack_mobility(asapRes, alapRes)
+    
+#     #Start with ASAP scheduling and check the total memory of each level (which is the TotalMemory attribute of each node on that level added together)
+#     #Then by using the slackRes, move around the nodes on the graph so each level has the minimum amount per level as possible
+#     pass
+
+# def minimize_latency_under_memory(G, M, L):
+#     #impliment latency minimization under memory M here
+#     asapRes = calculate_asap(G, L)
+#     alapRes = calculate_alap(G, L)
+#     slackRes = compute_slack_mobility(asapRes, alapRes)
+    
+#     #Start with ASAP scheduling and check the total memory of each level (which is the TotalMemory attribute of each node on that level added together)
+#     #Then by using SlackRes levels, move around the nodes and their dependents so each level is less or equal to M but also at the least level possible
+#     #If no version has the total memory of each level <= M then you get a warning message instead
+#     pass
 
 def minimize_memory_under_latency(G, L):
     #Get the ASAP, ALAP and the Slack = ALAP-ASAP scheduling for different node "levels"
@@ -227,37 +229,10 @@ def minimize_latency_under_memory(G, M, L):
                         asap_schedule[node] = new_level
 
     return asap_schedule
-  
-#TODO: GO OVER THIS AND CHECK IT'S OUTPUTS  
-def latency_memory_pareto_analysis(G, max_latency, max_memory):
-    pareto_results = {}
-    for latency in range(1, max_latency + 1):
-        for memory in range(1, max_memory + 1):
-            try:
-                mem_min_schedule = minimize_memory_under_latency(G, latency)
-                lat_min_schedule = minimize_latency_under_memory(G, memory, latency)
-                pareto_results[(latency, memory)] = (mem_min_schedule, lat_min_schedule)
-            except Exception:
-                pass
-
-    # Extract Pareto-optimal solutions
-    pareto_optimal_solutions = []
-    for latency_memory, schedules in pareto_results.items():
-        mem_min_schedule, lat_min_schedule = schedules
-        is_pareto_optimal = True
-
-        for other_latency_memory, other_schedules in pareto_results.items():
-            other_mem_min_schedule, other_lat_min_schedule = other_schedules
-            if (other_latency_memory != latency_memory and
-                    max(other_mem_min_schedule.values()) <= max(mem_min_schedule.values()) and
-                    max(other_lat_min_schedule.values()) <= max(lat_min_schedule.values())):
-                is_pareto_optimal = False
-                break
-
-        if is_pareto_optimal:
-            pareto_optimal_solutions.append((latency_memory, schedules))
-
-    return pareto_optimal_solutions
+    
+def latency_memory_pareto_analysis(G, L, M):
+    #implement Pareto-optimal analysis here
+    pass
 
 def calculate_asap(G, latency):
     topo_order = list(nx.topological_sort(G))
@@ -301,15 +276,12 @@ def main():
     G, rootNode = preprocess_graph(G)
     
     ilp_formulation = generate_ilp_formulation(G, args.l, args.a)
-    
-    memSolution_file, return_code = solve_ilp_formulation(ilp_formulation[0], True)
-    latSolution_file, return_code = solve_ilp_formulation(ilp_formulation[1], False)
-    
+    solution_file, return_code = solve_ilp_formulation(ilp_formulation)
+
     if return_code == 0:
-        print(f"memSolution file: {memSolution_file}")
-        print(f"latSolution file: {latSolution_file}")
+        print(f"Solution file: {solution_file}")
     else:
         print("Infeasible solution.")
 
 if __name__ == "__main__":
-    main()
+    main() 
